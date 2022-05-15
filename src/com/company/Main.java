fc063fbfdd673a5fcaba5d996b7944123d5c7f15
Svetlanaask <svetlanaaskaryar@gmail.com>
2022-05-15 14:31:54 -0400

    The feature Sun has been added succefully

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,13 @@ public class Main {
         FactorX.moon="4";
         FactorX.Planet="2";
         FactorX.Stars="1000";
+
+        //adding features to our sun
+        FeatureSun Xfact=new FeatureSun();
+        Xfact.color="green";
+        Xfact.radius="1 000 000 000 km";
+        Xfact.heat="2548963kj";
+
     }
 
 }
